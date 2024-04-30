ac613007651ea9c5df08fc0be3a0774421980139
vdevic01 <vlada.devic.2001@gmail.com>
2024-04-30 14:23:42 +0200

    Added color palette and rgb coloring

--- MandelbrotSetSequential/main.cpp
+++ MandelbrotSetSequential/main.cpp
@@ -4,17 +4,63 @@
 
 using namespace std;
 
-const double RE_START = -2.0;
-const double RE_END = 1.0;
-const double IM_START = -1;
-const double IM_END = 1;
+//const double RE_START = -2.0;
+//const double RE_END = 1.0;
+//const double IM_START = -1;
+//const double IM_END = 1;
+const double RE_START = -0.153004885037500013708;
+const double RE_END = -0.152809695287500013708;
+const double IM_START = 1.039611370300000000002;
+const double IM_END = 1.039757762612500000002;
 
-const int MAX_ITER = 150;
+const int MAX_ITER = 1000;
 
-const int IMAGE_WIDTH = 900;
-const int IMAGE_HEIGHT = 600;
+const int IMAGE_WIDTH = 1500;
+const int IMAGE_HEIGHT = 1000;
 
-const int PALETTE_LENGTH = 500;
+const int PALETTE_LENGTH = 1000;
+
+struct Color {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
+
+class ColorPalette {
+public:
+    vector<Color> colors;
+    double length;
+
+    ColorPalette(vector<Color> colors, int length) {
+        this->colors = colors;
+        this->length = length;
+    }
+
+    Color fit(int val) {
+        double valAdj = (double)(val % (int)this->length);
+        const int N = this->colors.size();
+        const double STEP = this->length / (N - 1);
+        Color* left = nullptr;
+        Color* right = nullptr;
+        double ratio;
+        for (int i = 0; i < N; i++) {
+            if (STEP * i > valAdj) {
+                left = &this->colors[i - 1];
+                right = &this->colors[i];
+                ratio = (valAdj - STEP * (i - 1)) / STEP;
+                break;
+            }
+        }
+        if (left == nullptr || right == nullptr) {
+            exit(2);
+        }
+        Color* output = new Color();
+        output->red = (int)(left->red + (right->red - left->red) * ratio);
+        output->green = (int)(left->green + (right->green - left->green) * ratio);
+        output->blue = (int)(left->blue + (right->blue - left->blue) * ratio);
+        return *output;
+    }
+};
 
 void createGrayscaleImage(int* pixels) {
     cv::Mat image(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC1);
@@ -31,6 +77,23 @@ void createGrayscaleImage(int* pixels) {
     //cv::waitKey(0);
 }
 
+void createColorImage(Color* pixels) {
+    cv::Mat image(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3);
+
+    for (int y = 0; y < IMAGE_HEIGHT; ++y) {
+        for (int x = 0; x < IMAGE_WIDTH; ++x) {
+            Color pixel_value = pixels[y * IMAGE_WIDTH + x];
+
+            cv::Vec3b& pixel = image.at<cv::Vec3b>(y, x); // Access the pixel at (x, y)
+            pixel[0] = pixel_value.blue; // Blue channel
+            pixel[1] = pixel_value.green; // Green channel
+            pixel[2] = pixel_value.red; // Red channel
+        }
+    }
+
+    cv::imwrite("mandelbrot_set.tiff", image);
+}
+
 double mapVal(double value, double inMin, double inMax, double outMin, double outMax) {
     return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
@@ -43,11 +106,36 @@ int complexPointIter(complex<double>& c) {
             return i;
         }
     }
-    return MAX_ITER;
+    return -1;
 }
 
-int main() {
-    int* pixels = new int[IMAGE_HEIGHT * IMAGE_WIDTH];
+void createMandelbrotSet() {
+    Color* pixels = new Color[IMAGE_HEIGHT * IMAGE_WIDTH];
+
+    vector<Color> colors;
+
+    Color c1;
+    c1.red = 7;
+    c1.green = 6;
+    c1.blue = 38;
+    Color c2;
+    c2.red = 240;
+    c2.green = 43;
+    c2.blue = 213;
+
+    //Color c1;
+    //c1.red = 20;
+    //c1.green = 170;
+    //c1.blue = 20;
+    //Color c2;
+    //c2.red = 255;
+    //c2.green = 255;
+    //c2.blue = 255;
+
+    colors.push_back(c1);
+    colors.push_back(c2);
+    colors.push_back(c1);
+    ColorPalette palette(colors, PALETTE_LENGTH);
 
     for (int i = 0; i < IMAGE_HEIGHT; i++) {
         for (int j = 0; j < IMAGE_WIDTH; j++) {
@@ -56,14 +144,39 @@ int main() {
             complex<double> point(realPart, imaginaryPart);
             int totalIters = complexPointIter(point);
             int idx = j + (i * IMAGE_WIDTH);
-
             if (totalIters == -1) {
-                pixels[idx] = 0;
+                Color black;
+                black.red = 0;
+                black.green = 0;
+                black.blue = 0;
+                pixels[idx] = black;
             }
-            else {               
-                pixels[idx] = ceil(mapVal(totalIters, MAX_ITER, 0, 0, 255));
+            else {
+                pixels[idx] = palette.fit(totalIters);
             }
+            
+            //pixels[idx] = ceil(mapVal(totalIters, MAX_ITER, 0, 0, 255));
         }
     }
-    createGrayscaleImage(pixels);
+    createColorImage(pixels);
+}
+
+void createColorPalette() {
+    vector<Color> colors;
+    Color c1;
+    c1.red = 252;
+    c1.green = 186;
+    c1.blue = 3;
+    Color c2;
+    c2.red = 111;
+    c2.green = 179;
+    c2.blue = 242;
+    colors.push_back(c1);
+    colors.push_back(c2);
+    colors.push_back(c1);
+    ColorPalette palette(colors, 250);
+}
+
+int main() {
+    createMandelbrotSet();
 }
