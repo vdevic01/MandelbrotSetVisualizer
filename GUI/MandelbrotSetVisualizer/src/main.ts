ad4085a781ee38c6db5d500e2e7b044108334a73
vdevic01 <vlada.devic.2001@gmail.com>
2024-05-12 19:23:07 +0200

    Bugfix

--- GUI/MandelbrotSetVisualizer/src/main.ts
+++ GUI/MandelbrotSetVisualizer/src/main.ts
@@ -36,9 +36,9 @@ let boundary: Boundary = {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  let boxX = -1;
-  let boxY = -1;
-  const boxRatio = [9, 16];
+  let pressX = -1;
+  let pressY = -1;
+  const boxRatio = [2, 3];
 
   let isPressed = false;
 
@@ -57,8 +57,8 @@ window.addEventListener("DOMContentLoaded", () => {
         p5.background(255);
       }
       if(isPressed){
-        let width = p5.mouseX - boxX;
-        let height = p5.mouseY - boxY;
+        let width = p5.mouseX - pressX;
+        let height = p5.mouseY - pressY;
         if(p5.abs(width) * boxRatio[0] > p5.abs(height) * boxRatio[1]){
           height = Math.sign(height) * p5.abs(width) * 1.0 / boxRatio[1] * boxRatio[0];
         }else{
@@ -68,38 +68,63 @@ window.addEventListener("DOMContentLoaded", () => {
         p5.noFill();
         p5.stroke(255);
         p5.strokeWeight(4);
-        p5.rect(boxX, boxY, width, height);
+        p5.rect(pressX, pressY, width, height);
         p5.stroke(0);
         p5.strokeWeight(2);
-        p5.rect(boxX, boxY, width, height);
+        p5.rect(pressX, pressY, width, height);
       }
     }
 
     p5.mousePressed = () => {
-      boxX = p5.mouseX;
-      boxY = p5.mouseY;
-      isPressed = true;
+      if(p5.mouseButton == p5.LEFT){
+        pressX = p5.mouseX;
+        pressY = p5.mouseY;
+        isPressed = true;
+      }
     }
 
     p5.mouseReleased = () => {
+      if(!isPressed){
+        return;
+      }
       isPressed = false;
 
-      let width = p5.mouseX - boxX;
-      let height = p5.mouseY - boxY;
-      let reStart = Math.min(boxX + width, boxX);
-      let reEnd = Math.max(boxX + width, boxX);
-      let imStart = Math.max(boxY + height, boxY);
-      let imEnd = Math.min(boxY + height, boxY);
+      let relX;
+      let relY;
+
+      let width = p5.mouseX - pressX;
+      let height = p5.mouseY - pressY;
+      console.log("mouseX, mouseY:" + [p5.mouseX, p5.mouseY]);
+      if(p5.abs(width) * boxRatio[0] > p5.abs(height) * boxRatio[1]){
+        relX = p5.mouseX;
+        relY = pressY + Math.sign(height) * p5.abs(width) * 1.0 / boxRatio[1] * boxRatio[0];
+      }else{
+        relX = pressX + Math.sign(width) * p5.abs(height) * 1.0 / boxRatio[0] * boxRatio[1];
+        relY = p5.mouseY;
+        console.log([relX, relY]);
+      }
+
+      let reStart = Math.min(pressX, relX);
+      let reEnd = Math.max(pressX, relX);
+      let imStart = Math.max(pressY, relY);
+      let imEnd = Math.min(pressY, relY);
+      const k1: number = (reEnd - reStart) / (imEnd - imStart);
+      console.log("Boundary side ratio (k1):" + k1);
+
       reStart = p5.map(reStart, 0, p5.width, boundary.reStart, boundary.reEnd);
       reEnd = p5.map(reEnd, 0, p5.width, boundary.reStart, boundary.reEnd);
       imStart = p5.map(imStart, p5.height, 0, boundary.imStart, boundary.imEnd);
       imEnd = p5.map(imEnd, p5.height, 0, boundary.imStart, boundary.imEnd);
+
       boundary = {
         reStart: reStart,
         reEnd: reEnd,
         imStart: imStart,
         imEnd: imEnd
       };
+      console.log(boundary);
+      const k2: number = (boundary.reEnd - boundary.reStart) / (boundary.imEnd - boundary.imStart);
+      console.log("Boundary side ratio (k2):" + k2);
       generateMandelbrot(boundary);
     }
 
